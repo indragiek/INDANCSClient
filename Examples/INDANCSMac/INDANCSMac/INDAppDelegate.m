4d949a3acbff89935618fa2bd75e750aadf51974
Indragie Karunaratne <i@indragie.com>
2013-12-22 20:40:13 -0700

    Basic UI for Mac app

--- Examples/INDANCSMac/INDANCSMac/INDAppDelegate.m
+++ Examples/INDANCSMac/INDANCSMac/INDAppDelegate.m
@@ -11,18 +11,44 @@
 
 @interface INDAppDelegate () <INDANCSClientDelegate>
 @property (nonatomic, strong) INDANCSClient *client;
+@property (nonatomic, weak) IBOutlet NSTableView *tableView;
+@property (nonatomic, strong) NSMutableArray *notifications;
 @end
 
 @implementation INDAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-	self.client = [INDANCSClient new];
+	self.notifications = [NSMutableArray array];
+	self.client = [[INDANCSClient alloc] init];
 	self.client.delegate = self;
 	[self.client scanForDevices:^(INDANCSClient *client, INDANCSDevice *device) {
-		NSLog(@"Found %@", device);
+		NSLog(@"Found %@", device.name);
 		[client registerForNotificationsFromDevice:device withBlock:^(INDANCSClient *c, INDANCSNotification *n) {
-			NSLog(@"Received notification: %@", n);
+			switch (n.latestEventID) {
+				case INDANCSEventIDNotificationAdded:
+					[self.notifications insertObject:n atIndex:0];
+					[self.tableView insertRowsAtIndexes:[NSIndexSet indexSetWithIndex:0] withAnimation:NSTableViewAnimationSlideLeft];
+					break;
+				case INDANCSEventIDNotificationRemoved: {
+					NSUInteger index = [self.notifications indexOfObject:n];
+					if (index != NSNotFound) {
+						[self.notifications removeObjectAtIndex:index];
+						[self.tableView removeRowsAtIndexes:[NSIndexSet indexSetWithIndex:index] withAnimation:NSTableViewAnimationSlideLeft];
+					}
+					break;
+				}
+				case INDANCSEventIDNotificationModified: {
+					NSUInteger index = [self.notifications indexOfObject:n];
+					if (index != NSNotFound) {
+						[self.notifications replaceObjectAtIndex:index withObject:n];
+						[self.tableView reloadDataForRowIndexes:[NSIndexSet indexSetWithIndex:index] columnIndexes:[NSIndexSet indexSetWithIndex:0]];
+					}
+					break;
+				}
+				default:
+					break;
+			}
 		}];
 	}];
 }
@@ -42,6 +68,18 @@
 - (void)ANCSClient:(INDANCSClient *)client serviceDiscoveryFailedForDevice:(INDANCSDevice *)device withError:(NSError *)error
 {
 	NSLog(@"Service discovery failed for %@ with error %@", device.name, error);
+}
+
+#pragma mark - NSTableViewDataSource
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView
+{
+	return self.notifications.count;
+}
+
+- (id)tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex
+{
+	return self.notifications[rowIndex];
 }
 
 @end
